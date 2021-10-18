2d8ba29351360f63f03c31587dc3691b5fc6bf10
jmguevarra <jaimeguevarra22@gmail.com>
2021-10-18 17:05:15 +0800

    Templating Layout and Content

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,43 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<body>
-    <header class="main-header py-3">
-        <div class="container">
-            <div class="row justify-content-center">
-                <h1 class="font-weight-bold">Todos Page</h1>
-            </div>
-        </div>
-    </header>
+@extends('layouts.app')
+  
 
-    <div class="todos-section py-3">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-8">
-                    <div class="card card-default">
-                        <div class="card-header">
-                            Todos
-                        </div>
-                        <div class="card-body">
-                            <div class="todos-lists list-group">
-                                @foreach($todos as $todo)
-                                    <div class="todo-list list-group-item d-flex justify-content-between">
-                                        <div class="todo-name">{{ $todo->name }}</div>
-                                        <a href="todos/{{ $todo->id }}" class="btn btn-primary btn-sm">View</a>
-                                    </div>
-                                @endforeach
+@section('page-title')
+    Todos List
+@endsection
+
+@section('content')
+<div class="todos-section py-3">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">
+                    Todos
+                </div>
+                <div class="card-body">
+                    <div class="todos-lists list-group">
+                        @foreach($todos as $todo)
+                            <div class="todo-list list-group-item d-flex justify-content-between">
+                                <div class="todo-name">{{ $todo->name }}</div>
+                                <a href="todos/{{ $todo->id }}" class="btn btn-primary btn-sm">View</a>
                             </div>
-                        </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
-    </div> 
-</body>
-</html>
+    </div>
+</div> 
+@endsection
